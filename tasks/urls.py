bb61f49aa5d87c6d9630c7aaea84f71237e23606
liangwater <liangwater@163.com>
2025-10-11 14:40:14 +0800

    获取数据

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from stats.views import CollectArticalView
 from .views import SimpleTaskViewSet, TaskTagsView, GlobalTagsView
 
 router = DefaultRouter()
@@ -9,6 +11,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('tags/global/', GlobalTagsView.as_view(), name='all-tags'),
     path('<int:task_id>/tags/', TaskTagsView.as_view(), name='task-tags'),
+    path('collect-tweets/', CollectArticalView.as_view(), name='collect-tweets'),
+
 ]
 
 
